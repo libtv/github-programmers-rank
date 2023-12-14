71414a5ef16e416d0f8be2aca85b8322b79a4265
baejeu12 <baejeu@naver.com>
2023-12-14 22:24:12 +0900

    commit: 프로그래머스 level0 문자열 붙여서 출력하기

---
+++ programmers/level0/PrintByPastingaString.java
@@ -0,0 +1,14 @@
+package org.example.programmers.level0;
+
+import java.util.Scanner;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181946
+// 문자열 붙여서 출력하기
+public class PrintByPastingaString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
