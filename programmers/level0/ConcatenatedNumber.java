f4e412ded73afcada7da1f32237b703eae0dfc9a
baejeu12 <baejeu@naver.com>
2023-12-15 02:30:42 +0900

    commit: 프로그래머스 level0 이어 붙인 수

---
+++ programmers/level0/ConcatenatedNumber.java
@@ -0,0 +1,25 @@
+package org.example.programmers.level0;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181928
+// 이어 붙인 수
+public class ConcatenatedNumber {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[] num_list) {
+
+        StringBuilder Odd = new StringBuilder();  // 홀수
+        StringBuilder Even = new StringBuilder(); // 짝수
+        for (int i = 0; i < num_list.length; i++) {
+            if(num_list[i]%2==0){
+                Even.append(num_list[i]);
+            }
+            else{
+                Odd.append(Integer.toString(num_list[i]));
+            }
+        }
+
+        return Integer.parseInt(Even.toString()) + Integer.parseInt(Odd.toString());
+
+    }
+}
