9666f402f53c5e3ccdbbddd0537f1d83d2be07bd
baejeu12 <baejeu@naver.com>
2023-12-15 01:12:06 +0900

    commit: 프로그래머스 level0 n의 배수

---
+++ programmers/level0/MultipleOfN.java
@@ -0,0 +1,19 @@
+package org.example.programmers.level0;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181937
+// n의 배수
+public class MultipleOfN {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int num, int n) {
+        if(num%n==0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+}
