b45691a846420918067c12af2966c0d7bbdf6727
baejeu12 <baejeu@naver.com>
2023-12-14 22:18:58 +0900

    commit: 프로그래머스 level0 덧셈식 출력하기

---
+++ programmers/level0/PrintingAdditionExpressions.java
@@ -0,0 +1,14 @@
+package org.example.programmers.level0;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181947
+// 덧셈식 출력하기
+
+import java.util.Scanner;
+public class PrintingAdditionExpressions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + " + " + b + " = " + (a+b));
+    }
+}
