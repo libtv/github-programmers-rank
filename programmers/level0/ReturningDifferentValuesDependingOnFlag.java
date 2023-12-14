1a33184182babe187243ca91c9a2497bd51f6352
baejeu12 <baejeu@naver.com>
2023-12-15 01:36:23 +0900

    commit: 프로그래머스 level0 flag에 따라 다른 값 반환하기

---
+++ programmers/level0/ReturningDifferentValuesDependingOnFlag.java
@@ -0,0 +1,12 @@
+package org.example.programmers.level0;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181933
+// flag에 따라 다른 값 반환하기
+public class ReturningDifferentValuesDependingOnFlag {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int a, int b, boolean flag) {
+        return flag ? a+b : a-b ;
+    }
+}
