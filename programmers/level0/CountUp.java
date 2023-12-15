a5305e0abec95b9eafd8c7fef8242a5d3ce6718b
baejeu12 <baejeu@naver.com>
2023-12-15 20:49:03 +0900

    commit: 프로그래머스 level0 카운트 업

---
+++ programmers/level0/CountUp.java
@@ -0,0 +1,17 @@
+package org.example.programmers.level0;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181920
+// 카운트 업
+public class CountUp {
+
+    public int[] solution(int start_num, int end_num) {
+      int [] answer = new int [end_num-start_num +1];
+      int j = 0;
+      for (int i = start_num; i <= end_num; i++) {
+            answer[j] = i;
+            j++;
+        }
+      return answer;
+    }
+
+}
