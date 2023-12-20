6ed4eceb6a5c583a4a879332c64452e9d1ff905c
baejeu12 <baejeu@naver.com>
2023-12-20 22:42:00 +0900

    commit: 프로그래머스 level1 덧칠하기

---
+++ programmers/level1/ApplyMorePaint.java
@@ -0,0 +1,34 @@
+package org.example.programmers.level1;
+
+import java.util.ArrayList;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/161989
+// 덧칠하기
+public class ApplyMorePaint {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int n, int m, int[] section) {
+        int cnt = 0; // 페인트
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < section.length; i++){
+            list.add(section[i]);
+        }
+        // System.out.println(list);
+        while(list.size() > 0){
+            int start = list.get(0);
+            for(int i= start; i < start + m ;i++){
+                if(list.contains(i)){
+                    int index = list.indexOf(i);
+                    list.remove(index);
+                }
+            }
+            cnt++;
+        }
+
+        return cnt;
+    }
+
+}
