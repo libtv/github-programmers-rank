a4d339ad6ad4b3072de5746dcecc0b997c090cbc
baejeu12 <baejeu@naver.com>
2023-12-22 00:49:01 +0900

    commit: 프로그래머스 level1 명예의 전당(1)

---
+++ programmers/level1/HallOfFame_1.java
@@ -0,0 +1,39 @@
+package org.example.programmers.level1;
+// https://school.programmers.co.kr/learn/courses/30/lessons/138477
+// 명예의 전당 (1)
+
+import java.util.*;
+public class HallOfFame_1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int k, int[] score) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for(int i = 0; i < score.length; i++){
+            if(list.size() < k){
+                list.add(score[i]);
+                list2.add(Collections.min(list));
+            }
+            else if(list.size() >= k){
+                if(Collections.min(list) < score[i]){
+                    int index = list.indexOf(Collections.min(list));
+                    list.remove(index);
+                    list.add(score[i]);
+                    list2.add(Collections.min(list));
+                }
+                else{
+                    list2.add(Collections.min(list));
+                }
+            }
+        }
+        int [] answer = new int[list2.size()];
+        for(int i = 0; i < list2.size(); i++){
+            answer[i] = list2.get(i);
+        }
+        return answer;
+    }
+}
+
+
