5621a9f903c3d5dc738f364e263ce71b7e705347
baejeu12 <baejeu@naver.com>
2023-12-22 22:23:06 +0900

    commit: 프로그래머스 level1 옹알이2

---
+++ programmers/level1/Babbling_2.java
@@ -0,0 +1,51 @@
+package org.example.programmers.level1;
+
+import java.util.ArrayList;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/133499
+// 옹알이 (2)
+public class Babbling_2 {
+    static ArrayList<String> list = new ArrayList<>();
+
+    static int result = 0;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(String[] babbling) {
+        list.add("aya");
+        list.add("ye");
+        list.add("woo");
+        list.add("ma");
+        for (int i = 0; i < babbling.length ; i++) {
+            String str = babbling[i];
+            int babblingLength = babbling[i].length();
+            for (int j = 0; j < 4 ; j++) {
+                String strCheck = list.get(j) + list.get(j);
+                if(str.contains(strCheck)){
+                    break;
+                }
+                babblingLength = babblingLength - test(str, j);
+            }
+            if(babblingLength==0){
+                result++;
+            }
+        }
+        return result;
+
+    }
+
+    public static int test(String str , int cnt){
+        int returnInt = 0;
+        int length = list.get(cnt).length();
+        while(str.contains(list.get(cnt))){
+            int check = str.indexOf(list.get(cnt));
+            str = str.substring(0, check) + str.substring(check+ length);
+            returnInt ++;
+        }
+        return returnInt * length;
+    }
+
+
+}
