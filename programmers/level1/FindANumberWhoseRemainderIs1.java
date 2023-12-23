8cc7c0f807fda0ffe58e758edf7e091acd575323
baejeu12 <baejeu@naver.com>
2023-12-23 17:14:15 +0900

    commit: 프로그래머스 level1 나머지가 1이 되는 수 찾기

---
+++ programmers/level1/FindANumberWhoseRemainderIs1.java
@@ -0,0 +1,17 @@
+package org.example.programmers.level1;
+// https://s나머지가 1이 되는 수 찾기chool.programmers.co.kr/learn/courses/30/lessons/87389
+// 나머지가 1이 되는 수 찾기
+public class FindANumberWhoseRemainderIs1 {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int n) {
+
+        for(int i = 1; i < n; i++){
+            if(n % i ==1){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
