0499275d2f57693f9496ce288eb1feeb6be99153
baejeu12 <baejeu@naver.com>
2023-12-23 17:18:39 +0900

    commit: 프로그래머스 level1 없는 숫자 더하기

---
+++ programmers/level1/AddMissingNumbers.java
@@ -0,0 +1,16 @@
+package org.example.programmers.level1;
+// https://school.programmers.co.kr/learn/courses/30/lessons/86051
+// 없는 숫자 더하기
+public class AddMissingNumbers {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[] numbers) {
+
+        int sum = 0;
+        for(int i =0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        return 45-sum;
+    }
+}
