0ce252e458606a419ef4631c81a8407a453c14c0
baejeu12 <baejeu@naver.com>
2023-12-28 20:53:17 +0900

    commit: 프로그래머스 level1 짝수와 홀수

---
+++ programmers/level1/EvenAndOddNumbers.java
@@ -0,0 +1,17 @@
+package org.example.programmers.level1;
+// https://school.programmers.co.kr/learn/courses/30/lessons/12937
+// 짝수와 홀수
+public class EvenAndOddNumbers {
+    public static void main(String[] args) {
+
+    }
+    public String solution(int num) {
+        if(num%2==0){
+            return "Even";
+        }
+        else{
+            return "Odd";
+        }
+    }
+
+}
