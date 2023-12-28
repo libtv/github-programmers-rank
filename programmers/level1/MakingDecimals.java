f6b39f0185fe156d2d617e6b964b1dc43bc633ad
baejeu12 <baejeu@naver.com>
2023-12-29 01:38:22 +0900

    commit: 프로그래머스 level1 소수 만들기

---
+++ programmers/level1/MakingDecimals.java
@@ -0,0 +1,40 @@
+package org.example.programmers.level1;
+
+import java.util.ArrayList;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12977
+// 소수 만들기
+public class MakingDecimals {
+    public static void main(String[] args) {
+
+
+    }
+    public int solution(int[] nums) {
+        int cnt = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                for(int k = j+1; k < nums.length; k++){
+                    if(find(nums[i]+nums[j]+nums[k])){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+    public static boolean find(int sum){
+        boolean flag = false;
+        for(int i = 2; i <= Math.sqrt(sum); i++){
+            if(sum%i==0){
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            return false;
+        }
+        return true;
+    }
+
+}
+
