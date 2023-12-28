7ec1c23cc26c26031d4676626cea484a5fe3554c
baejeu12 <baejeu@naver.com>
2023-12-28 17:57:53 +0900

    commit: 프로그래머스 level1 소수찾기

---
+++ programmers/level1/FindPrimeNumbers.java
@@ -0,0 +1,27 @@
+package org.example.programmers.level1;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12921
+// 소수 찾기
+public class FindPrimeNumbers {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i <= n; i++){
+            int cnt = 0;
+            for(int j = 2; j <= Math.sqrt(i); j++){
+                if(i%j==0){
+                    cnt++;
+                    break;
+                }
+            }
+            if(cnt == 0){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+}
