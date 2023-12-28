9f9505371068aad8228cc69d74d1ab6471309348
baejeu12 <baejeu@naver.com>
2023-12-28 20:50:56 +0900

    commit: 프로그래머스 level1 정수 제곱근 판별

---
+++ programmers/level1/DetermineIntegerSquareRoot.java
@@ -0,0 +1,33 @@
+package org.example.programmers.level1;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12934
+// 정수 제곱근 판별
+public class DetermineIntegerSquareRoot {
+    public static void main(String[] args) {
+
+    }
+    public long solution(long n) { // 100점
+        double temp = Math.sqrt(n);
+        String tmp = Double.toString(temp);
+        String[] arr = tmp.split("\\.");
+        // System.out.println(arr[1]);
+        if(arr[1].length() == 1){
+            return (long)Math.pow(temp + 1 , 2);
+        }
+        return -1;
+    }
+
+}
+/* 83.3점
+ public long solution(long n) {
+        double nn   = (double) n;
+        double temp = Math.sqrt(n);
+        System.out.println(temp);
+        if(nn == Math.pow(temp , 2)){
+            return (long) Math.pow(temp+1, 2);
+        }
+        else{
+            return -1;
+        }
+    }
+ */
