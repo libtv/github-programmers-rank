0150e86d255c1c195a6f5cf16312b3316aa560fc
baejeu12 <baejeu@naver.com>
2023-12-28 15:39:40 +0900

    commit: 프로그래머스 level1 완전탐색 최소직사삭형

--- programmers/level1/MinimumRectangle.java
+++ programmers/level1/MinimumRectangle.java
@@ -60,3 +60,14 @@ public class MinimumRectangle {
         }
     }
 }
+/* 어떻게 했노..
+ public int solution(int[][] sizes) {
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
+        }
+        int answer = length * height;
+        return answer;
+    }
+ */
