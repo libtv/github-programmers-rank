b7222e3d736f4c78fce80775c1ea7a16182adc9d
baejeu12 <baejeu@naver.com>
2023-12-28 21:11:21 +0900

    commit: 프로그래머스 level1 하샤드 수

---
+++ programmers/level1/HarshadNumber.java
@@ -0,0 +1,21 @@
+package org.example.programmers.level1;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12947
+// 하샤드 수
+public class HarshadNumber {
+    public static void main(String[] args) {
+
+    }
+    public boolean solution(int x) {
+        boolean answer = true;
+        String temp = Integer.toString(x);
+        int sum = 0;
+        for(int i = 0; i < temp.length(); i++){
+            sum+= temp.charAt(i) - '0';
+        }
+        if(!(x%sum==0)) answer = false;
+
+        return answer;
+    }
+
+}
