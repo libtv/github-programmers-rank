06170d04fd7a3b4f654f04e570736ec7a9d0e6d5
baejeu12 <baejeu@naver.com>
2023-12-29 01:39:37 +0900

    commit: 프로그래머스 level1 소수 만들기 프로그래머스 level1 end!!!!!

--- programmers/level1/MakingDecimals.java
+++ programmers/level1/MakingDecimals.java
@@ -9,7 +9,7 @@ public class MakingDecimals {
 
 
     }
-    public int solution(int[] nums) {
+    public int solution(int[] nums) { // 조합의수
         int cnt = 0;
         for(int i = 0; i < nums.length; i++){
             for(int j = i+1; j < nums.length; j++){
