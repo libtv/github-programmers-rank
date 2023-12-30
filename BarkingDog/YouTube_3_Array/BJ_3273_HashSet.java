63fcbc069a12fa3fd669a4ae5df5e5c07dadb03a
baejeu12 <baejeu@naver.com>
2023-12-30 22:54:17 +0900

    commit: 바킹독 유투브 실전 알고리즘 3강 백준 3273 두 수의 합 HashSet 사용

---
+++ BarkingDog/YouTube_3_Array/BJ_3273_HashSet.java
@@ -0,0 +1,33 @@
+package org.example.BarkingDog.YouTube_3_Array;
+
+import java.util.*;
+
+// https://www.acmicpc.net/problem/3273
+// 두 수의 합
+// 실버 3
+public class BJ_3273_HashSet {
+    public static void main(String[] args) {
+        // list에서 contains로 요소를 찾는 방법은 O(N)이 걸린다. 하지만 Set에서 Contains는 O(1)이라는 시간만 걸린다를 이용
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n ; i++) {
+            list.add(sc.nextInt());
+        }
+        int sum = sc.nextInt();
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n ; i++) {
+            set.add(sum-list.get(i));
+        }
+        int cnt = 0;
+        for (int i = 0; i < n ; i++) {
+            if(set.contains(list.get(i))){
+                cnt++;
+            }
+        }
+        System.out.println(cnt/2);
+    }
+}
+// 112436 KB 956 ms
