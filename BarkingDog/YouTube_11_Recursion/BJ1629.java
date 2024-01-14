7029d2687b28d84b2da9f8ff19eaed7a02e60c95
baejeu12 <baejeu@naver.com>
2024-01-15 00:54:43 +0900

    commit: 바킹독 11강 재귀 백준 1629 곱셈

---
+++ BarkingDog/YouTube_11_Recursion/BJ1629.java
@@ -0,0 +1,43 @@
+package org.example.BarkingDog.YouTube_11_Recursion;
+
+// https://www.acmicpc.net/problem/1629
+// 곱셈 실버1
+import java.io.*;
+import java.util.*;
+public class BJ1629 {
+    static long C;
+    public static void main(String[] args) {
+        // 자연수 A를 B번 곱한 수를 알고 싶다. 단 구하려는 수가 매우 커질 수 있으므로 이를 C로 나눈 나머지를 구하는 프로그램을 작성하시오.
+        // 첫째 줄에 A, B, C가 빈 칸을 사이에 두고 순서대로 주어진다. A, B, C는 모두 2,147,483,647 이하의 자연수이다.
+
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        C = sc.nextLong();
+        System.out.println(find(A, B));
+
+
+    }
+
+    public static long find(long A, long B){ // 10 , 11 , 12
+        if(B==0){
+            return 0;
+        }
+        else if(B==1){
+            return A % C;
+        }
+        long num = find(A, B/2);
+
+        if(B%2 == 1){
+            return num * num % C * A % C; // 이건 통과  (num * num * A) % C 는 왜?
+        }
+        // B%2 == 0
+        return (num * num) % C;
+
+    }
+}
+/*
+(A + B) % p = ((A % p) + (B % p)) % p
+(A * B) % p = ((A % p) * (B % p)) % p  == > (a5 * a5) % c  == ((a5 % c) * (a5 %c)) % c
+(A - B) % p = ((A % p) - (B % p) + p) % p
+ */
