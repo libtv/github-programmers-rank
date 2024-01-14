fb6a4ce0abe309eadff9ade7ab0d02e4aee137c8
baejeu12 <baejeu@naver.com>
2024-01-15 00:22:01 +0900

    commit: 바킹독 11강 재귀 모듈러 법칙 공부

---
+++ BarkingDog/YouTube_11_Recursion/RecursionStudy.java
@@ -0,0 +1,21 @@
+package org.example.BarkingDog.YouTube_11_Recursion;
+
+import java.util.Map;
+
+public class RecursionStudy {
+    public static void main(String[] args) {
+        // 모듈러 법칙
+        System.out.println(((Math.pow(10, 5) * Math.pow(10, 5) * Math.pow(10,1)) % 12));
+        System.out.println( ((Math.pow(10, 5) % 12) * (Math.pow(10, 5)%12) * (Math.pow(10 , 1) % 12)) % 12);
+        System.out.println(Math.pow(10, 11) % 12);
+        /*
+        4.0
+        4.0
+        4.0
+         */
+        // 결론
+        // (a5 * a5 * a1) % 12 = ((a5 % 12) * (a5 % 12) * (a1 % 12)) % 12
+        // (a11) % 12 = ((a5 % 12) * (a5 % 12) * (a1 % 12)) % 12
+
+    }
+}
