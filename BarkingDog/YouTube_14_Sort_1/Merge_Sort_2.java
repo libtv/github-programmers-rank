029150fae5ea3a5a069e3c56c4bcd648bbadfe92
baejeu12 <baejeu@naver.com>
2024-01-23 14:35:08 +0900

    commit: 바킹독 14강 정렬_1 Merge_Sort 병합정렬 O(NlogN)으로 구현하기

---
+++ BarkingDog/YouTube_14_Sort_1/Merge_Sort_2.java
@@ -0,0 +1,55 @@
+package org.example.BarkingDog.YouTube_14_Sort_1;
+
+import java.util.Arrays;
+
+// Merge_Sort에서 짠 방식은 0(N+M) 시간복잡도이다. 2번만 연산 했으니
+// 이걸 1개리스트에서 시작해서 나누고 나누고 나누고 해서 합치고 합치고 합치고 하는 방식으로 하면 O(NlongN)으로 해결 가능하다.
+public class Merge_Sort_2 {
+    static int [] arr = {-3, -1, 4, -5, 7, 5, 3, 0, 2, 9, 8};
+    static int [] tmp = new int[arr.length];
+    public static void main(String[] args) {
+        func(0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void func(int sx, int ex){ // 나누기
+        if(ex - sx == 1){
+            return;
+        }
+        int mx = (ex + sx) / 2;
+        func(sx, mx);
+        func(mx ,ex);
+        func2(sx , ex);
+    }
+    public static void func2(int sx, int ex){ // 합치기
+        int mx = (sx + ex) / 2;
+        int cnt1 = sx;
+        int cnt2 = mx;
+        for (int i = sx; i < ex; i++) { // 0  2
+            if(cnt2 == ex){
+                tmp[i] = arr[cnt1];
+                cnt1++;
+            }
+            else if(cnt1 == mx){
+                tmp[i] = arr[cnt2];
+                cnt2++;
+            }
+            else{
+                if(arr[cnt2] < arr[cnt1]){
+                    tmp[i] = arr[cnt2];
+                    cnt2++;
+                }
+                else{
+                    tmp[i] = arr[cnt1];
+                    cnt1++;
+                }
+            }
+        }
+        for (int i = sx; i < ex ; i++) {
+            arr[i] = tmp[i];
+        }
+    }
+}
+
+// 0 1 2 3 4 의 5크기의 배열이 있다.
+// sx = 0 ex = 5
+
