4792620721010f0b5afeee5edd1b7801a47e8f78
baejeu12 <baejeu@naver.com>
2024-01-31 22:56:31 +0900

    commit: 바킹독 18강 Math 백준 11653 소인수분해 브론즈1

---
+++ BarkingDog/YouTube_18_Math/BJ11653.java
@@ -0,0 +1,28 @@
+package org.example.BarkingDog.YouTube_18_Math;
+
+import java.util.Scanner;
+
+// 소인수분해 브론즈1
+public class BJ11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        int cnt = 2;
+        int end = N;
+        while (cnt * cnt <= end){
+            if(N % cnt == 0){
+                N /= cnt;
+                sb.append(cnt);
+                sb.append("\n");
+            }
+            else{
+                cnt++;
+            }
+        }
+        if(N != 1){
+            sb.append(N);
+        }
+        System.out.println(sb);
+    }
+}
