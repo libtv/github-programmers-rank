7cd4953eca73a4ead9a74f3d92474f543ff5ad2b
baejeu12 <baejeu@naver.com>
2024-02-01 19:54:36 +0900

    commit: 바킹독 18강 Math 백준 2292 벌집 브론즈2

---
+++ BarkingDog/YouTube_18_Math/BJ2292.java
@@ -0,0 +1,33 @@
+package org.example.BarkingDog.YouTube_18_Math;
+
+import java.util.Scanner;
+
+// 벌집 브론즈2
+public class BJ2292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        long sum = 1;
+        int cnt = 0;
+        while (true){
+            sum += cnt * (6);
+            cnt++;
+            if(input <= sum){
+                break;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+/*
+1 : 1
+2 : 2 3 4 5 6 7
+3 : 8 9 10 11 12 13 14 15 16 17 18 19
+
+
+
+
+
+
+
+ */
