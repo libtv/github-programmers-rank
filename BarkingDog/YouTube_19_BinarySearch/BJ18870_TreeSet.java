0db890045c8c649d5729ca84a84a409608e65ec6
baejeu12 <baejeu@naver.com>
2024-02-03 15:53:23 +0900

    commit: 바킹독 19강 BinarySearch 백준 18870 좌표 압축 실버2 TreeSet 활용

---
+++ BarkingDog/YouTube_19_BinarySearch/BJ18870_TreeSet.java
@@ -0,0 +1,38 @@
+package org.example.BarkingDog.YouTube_19_BinarySearch;
+
+import java.io.*;
+import java.util.*;
+public class BJ18870_TreeSet {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        Set<Integer> set = new TreeSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while (n-->0){
+            int temp = Integer.parseInt(st.nextToken());
+            list.add(temp);
+            set.add(temp);
+        }
+        int cnt = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int target: set) {
+            map.put(target, cnt);
+            cnt++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(map.get(list.get(i))).append(" ");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+/*
+BOJ18870 TreeSet 활용해서 다시 풀기
+TreeSet은 HashSet과 다르게 정렬되어있다.
+
+ */
