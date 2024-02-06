586eb91f703a4507bbfd6d2e57bf4aa21a0554df
baejeu12 <baejeu@naver.com>
2024-02-06 18:06:10 +0900

    commit: 바킹독 20강 TwoPointer 백준 22682 List of Unique Numbers 골드4 다시 풀기

---
+++ BarkingDog/YouTube_20_TwoPointer/BJ13144_Upgrade.java
@@ -0,0 +1,32 @@
+package org.example.BarkingDog.YouTube_20_TwoPointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ13144_Upgrade {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] check = new int[100001];
+        int [] arr = new int[n + 1];
+        for (int i = 1; i <= n ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        long sum = 0;
+        int left = 0;
+        int right = 1;
+        while (left <= right && left <= n && right <= n){
+            int input = arr[right];
+            if (left < check[input]){
+                left = check[input];
+            }
+            check[input] = right;
+            sum += right - left;
+            right++;
+        }
+        System.out.println(sum);
+    }
+}
