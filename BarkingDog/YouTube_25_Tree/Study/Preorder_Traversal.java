8de9ad51c7eb1cc57adef8f34a9fa407029207bd
baejeu12 <baejeu@naver.com>
2024-02-15 14:39:57 +0900

    commit: 바킹독 25강 Tree 전위 순회(Preorder-Traversal)

---
+++ BarkingDog/YouTube_25_Tree/Study/Preorder_Traversal.java
@@ -0,0 +1,19 @@
+package org.example.BarkingDog.YouTube_25_Tree.Study;
+
+// 전위 순회
+public class Preorder_Traversal {
+    static int [] lc = {0,2,4,6,0,0,0,0,0};
+    static int [] rc = {0,3,5,7,0,8,0,0,0};
+    public static void main(String[] args) {
+        preorder(1);
+    }
+    public static void preorder(int x){ // 전위 순회
+        System.out.println(x);
+        if(lc[x] != 0){
+            preorder(lc[x]);
+        }
+        if(rc[x] != 0){
+            preorder(rc[x]);
+        }
+    }
+}
