7b1735e87e901fe38d13a82f0bd5fc0994994890
baejeu12 <baejeu@naver.com>
2024-02-25 03:23:54 +0900

    commit: 프로그래머스 level2 뒤에 있는 큰 수 찾기

---
+++ programmers/level2/FindTheNextBigNumber.java
@@ -0,0 +1,19 @@
+package org.example.programmers.level2;
+
+// 뒤에 있는 큰 수 찾기
+import java.util.*;
+public class FindTheNextBigNumber {
+    public int[] solution(int[] numbers) {
+        int [] answer = new int [numbers.length];
+        int size = numbers.length - 1;
+        Arrays.fill(answer, - 1);
+        Stack <Integer> stack = new Stack<>();
+        for(int i = 0; i <= size; i++){
+            while(!stack.isEmpty() && numbers[stack.peek()] < numbers[i]){
+                answer[stack.pop()] = numbers[i];
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
