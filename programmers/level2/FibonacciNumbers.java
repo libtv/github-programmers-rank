feb488a502688adcb0e71b9c0ce8ee6115b930ea
baejeu12 <baejeu@naver.com>
2024-02-29 16:18:45 +0900

    프로그래머스 Level2 피보나치 수

---
+++ programmers/level2/FibonacciNumbers.java
@@ -0,0 +1,14 @@
+package org.example.programmers.level2;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12945
+// 프로그래머스 Level2 피보나치 수
+public class FibonacciNumbers {
+    public int solution(int n) {
+        int [] DP = new int[n+1];
+        DP[1] = 1;
+        for(int i = 2; i <= n; i++){
+            DP[i] = (DP[i-1] + DP[i-2]) % 1234567;
+        }
+        return DP[n];
+    }
+}
