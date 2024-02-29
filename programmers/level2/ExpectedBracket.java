324468c3467ab5e3833db240b36e1772a45e996a
baejeu12 <baejeu@naver.com>
2024-02-29 22:37:54 +0900

    프로그래머스 Level2 예상 대진표

---
+++ programmers/level2/ExpectedBracket.java
@@ -0,0 +1,16 @@
+package org.example.programmers.level2;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12985
+// 예상 대진표
+public class ExpectedBracket {
+    public int solution(int n, int a, int b){
+        int cnt = 0;
+        while(a != b){
+            cnt++;
+            a++; b++;
+            a = a / 2;
+            b = b / 2;
+        }
+        return cnt;
+    }
+}
