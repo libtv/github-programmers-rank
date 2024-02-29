9a1e1b6c8955d59d3e9bc0348f6c5aba4782b533
baejeu12 <baejeu@naver.com>
2024-02-29 15:44:36 +0900

    프로그래머스 Level2 N개의 최소공배수

---
+++ programmers/level2/NleastCommonMultiples.java
@@ -0,0 +1,18 @@
+package org.example.programmers.level2;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12953
+// 프로그래머스 Level2 N개의 최소공배수
+public class NleastCommonMultiples {
+    public int solution(int[] arr) {
+        int start = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            int target = arr[i];
+            start = (start * target) / GCD(start, target);
+        }
+        return start;
+    }
+    public static int GCD(int a, int b){
+        if(b == 0) return a;
+        return GCD(b, a % b);
+    }
+}
