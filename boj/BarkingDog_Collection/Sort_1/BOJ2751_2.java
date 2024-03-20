aa4105a64f9c85abdb0023e2168c15c45da506d7
baejeu12 <baejeu@naver.com>
2024-03-20 12:45:00 +0900

    백준 2751 수 정렬하기2 실버5 Arrays.sort

---
+++ boj/BarkingDog_Collection/Sort_1/BOJ2751_2.java
@@ -0,0 +1,21 @@
+package boj.BarkingDog_Collection.Sort_1;
+
+import java.io.*;
+import java.util.*;
+// Arrays.sort() 사용하기 시간초과 -> StringBuilder로 출력 하니까 통과
+public class BOJ2751_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int [] arr = new int[n];
+        for (int i = 0; i < n ; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
