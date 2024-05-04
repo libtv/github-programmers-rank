b9815133a5704adedd0f60cb5ceafb2e3794faaa
baejeu12 <baejeu@naver.com>
2024-05-04 17:23:17 +0900

    백준 15984 수학은 체육과목 입니다 브론즈3

---
+++ boj/BarkingDog_Collection/Math/BOJ15894.java
@@ -0,0 +1,12 @@
+package boj.BarkingDog_Collection.Math;
+
+import java.util.Scanner;
+
+// 수학은 체육과목 입니다 브론즈3
+public class BOJ15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println((long) input * 4);
+    }
+}
