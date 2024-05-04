96f7e3b5f7bba4b5267dec2f3ea4015efafb8ac6
baejeu12 <baejeu@naver.com>
2024-05-04 15:20:15 +0900

    알고리즘 수학 소인수분해

---
+++ study/math/Factorization.java
@@ -0,0 +1,20 @@
+package study.math;
+
+import java.util.ArrayList;
+
+// 소인수 분해
+public class Factorization {
+    public static void main(String[] args) {
+        int target = 999;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 2; i * i <= target; i++) {
+            while (target % i == 0){
+                list.add(i);
+                target /= i;
+            }
+        }
+        if(target != 1) list.add(target);
+        System.out.println(list);
+    }
+
+}
