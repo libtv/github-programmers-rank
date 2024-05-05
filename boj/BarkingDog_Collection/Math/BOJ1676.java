bcd623dcd5643e9181fa9504302a6e57315815f4
jeus1998 <zeus20240228@gmail.com>
2024-05-05 12:04:26 +0900

    백준 1676 팩토리얼 0의 개수

---
+++ boj/BarkingDog_Collection/Math/BOJ1676.java
@@ -0,0 +1,32 @@
+package boj.BarkingDog_Collection.Math;
+
+// 팩토리얼 0의 개수 실버5
+import java.util.*;
+public class BOJ1676 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long cur = 1;
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            cur *= i;
+            while (cur % 10 == 0){
+                cnt++;
+                cur /= 10;
+            }
+            cur %= 1000;
+        }
+        System.out.println(cnt);
+    }
+}
+/*
+
+1 2 3 4 5 6 7 8 9
+
+10 10
+
+
+
+
+
+ */
