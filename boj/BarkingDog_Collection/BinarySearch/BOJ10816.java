3f991bf35879f13b01136acd38e81da62172f005
jeus1998 <zeus20240228@gmail.com>
2024-05-07 21:23:42 +0900

    백준 10816 숫자 카드 2 실버4

--- boj/BarkingDog_Collection/BinarySearch/BOJ10816.java
+++ boj/BarkingDog_Collection/BinarySearch/BOJ10816.java
@@ -4,53 +4,51 @@ package boj.BarkingDog_Collection.BinarySearch;
 import java.io.*;
 import java.util.*;
 public class BOJ10816 {
+    static int [] cards;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int [] cards = new int[n];
+        cards = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             cards[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(cards);
 
-        System.out.println(Arrays.toString(cards));
+        // System.out.println(Arrays.toString(cards));
 
         int t = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int target = Integer.parseInt(st.nextToken());
-
-            int left = 0;
-            int right = 0;
-
-            int start = 0;
-            int end   = n;
-            while (start < end){
-                int mid = (start + end) / 2;
-                if(cards[mid] <= target){
-                    start = mid + 1;
-                }
-                else end = mid;
-            }
-            right = end;
-
-            start = 0;
-            end   = n;
-            while (start < end){
-                int mid = (start + end) / 2;
-                if(cards[mid] >= target){
-                    end = mid;
-                }
-                // cards[mid] < target
-                else start = mid + 1;
-            }
-            left = start;
-
-            sb.append(right - left).append(" ");
+            sb.append(upper_idx(target) - lower_idx(target)).append(" ");
         }
         System.out.println(sb);
+    }
+    public static int lower_idx(int target){
+        int start = 0;
+        int end   = cards.length;
+        while (start < end){
+            int mid = (start + end) / 2;
+            if(cards[mid] < target){
+                start = mid + 1;
+            }
+            else end = mid;
+        }
+        return start;
+    }
+    public static int upper_idx(int target){
+        int start = 0;
+        int end   = cards.length;
+        while (start < end){
+            int mid = (start + end) / 2;
+            if(cards[mid] <= target){
+                start = mid + 1;
+            }
+            else end = mid;
+        }
+        return  end;
     }
 }
