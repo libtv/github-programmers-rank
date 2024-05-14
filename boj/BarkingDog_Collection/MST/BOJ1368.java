39abaa51e3c14c93067435b73551635bf6403cff
jeus1998 <zeus20240228@gmail.com>
2024-05-15 00:11:23 +0900

    백준 1368 물대기 골드2 - 프림

--- boj/BarkingDog_Collection/MST/BOJ1368.java
+++ boj/BarkingDog_Collection/MST/BOJ1368.java
@@ -3,7 +3,8 @@ package boj.BarkingDog_Collection.MST;
 /**
  * 물대기 골드2
  * MST
- * 13% -> 틀렸습니다
+ * 13%  -> 가장 우물 비용이 적은 우물에서 mst 시작
+ * 100% -> 가상의 정점 활용 (우물 = 0)
  */
 import java.util.*;
 import java.io.*;
